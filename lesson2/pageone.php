e975a03c40a2ebb1173af0107fdd353884c5783a
KamauAdrian <35832303+KamauAdrian@users.noreply.github.com>
2018-05-17 04:32:41 +0300

    lesson2

---
+++ lesson2/pageone.php
@@ -0,0 +1,9 @@
+<?php
+//Concatenation
+$day='Thursday';
+$month='August';
+$date='26th';
+$year='1999';
+echo 'I was born on'.$day.' the '.$date.' day of '.$month.' '.$year.'<br />';
+//Alternatively
+echo "I was born on $day the $date day of $month $year";
