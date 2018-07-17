00703825e614678698571fe4efc19d0ebd590c50
KamauAdrian <35832303+KamauAdrian@users.noreply.github.com>
2018-07-17 11:08:13 +0300

    laravel intro

---
+++ cms/resources/views/posts/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+    <form action="" method="post">
+        <input type="text" name="title" placeholder="Title">
+        <input type="submit" name="submit">
+    </form>
+@endsection
